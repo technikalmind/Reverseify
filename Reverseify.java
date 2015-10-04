0f50dae2431c9a5990722d922fff3889d6861329
George Hall <georgehdev@gmail.com>
2015-10-03 23:39:46 -0400

    Displays menu and accepts user input
    Added a menu with 3 options (Reverse integers, string, or exit). 
    
    When entering numbers, integers are temporarily stored in a linked list to allow user to enter as many numbers as they choose.

--- Reverseify.java
+++ Reverseify.java
@@ -42,7 +42,81 @@ public class Reverseify {
 		}
 	}
 	
+	public static int getMenu() {
+		Scanner sc = new Scanner(System.in);
+		String input = "";
+		System.out.println("Please select an option: ");
+		System.out.println(
+			"1) Reverse an array of integers\n" +
+			"2) Reverse a String\n" +
+			"3) Exit\n");
+		try {
+				input = sc.next();
+				if(input.equals("1")){
+					return 1;
+				} else if(input.equals("2")) {
+					return 2;						
+				} else if(input.equals("3")) {
+					return 3;
+				} else {
+					throw new IllegalArgumentException("Illegal argument: " + input +". Please choose a correct value");
+				}
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return -1;
+		}
+	}
+	
+	public static void getOption(int opt) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			switch(opt) {
+				case 1:
+					LinkedList<Integer> list = new LinkedList<Integer>();
+					System.out.println("Enter an integer (enter x to stop): ");
+					
+					while(true){
+						String input = sc.next();
+						if(input.equals("x")) {
+							break;
+						} else {
+							int n = Integer.parseInt(input);
+							list.add(n);
+						}
+					}
+					
+					int[] listArr = new int[list.size()];
+					
+					for(int i = 0; i <= list.size()-1; i++) {
+						listArr[i] = list.get(i);
+						System.out.print(list.get(i) + " ");
+					}
+					System.out.println();
+					int[] reverse = reverse(listArr);
+					printInt(reverse);
+					System.out.println();
+					break;
+				case 2:
+					System.out.println("Type a message to be reversed: ");
+					String s = sc.nextLine();
+					System.out.println(reverse(s));
+					break;
+				default:
+					break;
+			}
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
 	public static void main(String[] args) {
+		int menu;
+		do {
+			menu = getMenu();
+			getOption(menu);
+		} while(menu != 3);
+		
+		/*
 		int[] array = {1, 2, 3, 4};
 		int[] revInt = reverse(array);
 		
@@ -55,5 +129,6 @@ public class Reverseify {
 		String s = "hello world how are you today";
 		System.out.println(s);
 		System.out.println(reverse(s));
+		*/
 	}
 }
